07c7ae4f3f65948e8b1adad34eb18602cfacf975
Stefan Hyltoft <stefan@Stefans-iMac.local>
2019-01-05 16:07:52 +0100

    Got autocomplete with suggestions working

--- mobile/screens/Passenger.js
+++ mobile/screens/Passenger.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  TouchableHighlight,
+  Keyboard
+} from "react-native";
 import MapView from "react-native-maps";
+import apiKey from "../google_api_key";
+import _ from "lodash";
 
-// Show map... select location to go to
+//Show map... select location to go to
+//Get location route with Google Location API
 //Send driver request
 //Wait for driver to arrive
 //Get picked up by driver
 //Let driver drive to location
-//(who cares about money and price?... implemented later with Google location API)
 
 export default class Passenger extends Component {
   constructor(props) {
@@ -15,8 +24,13 @@ export default class Passenger extends Component {
     this.state = {
       error: "",
       latitude: 0,
-      longitude: 0
+      longitude: 0,
+      locationPredictions: []
     };
+    this.onChangeDestinationDebounced = _.debounce(
+      this.onChangeDestination,
+      1000
+    );
   }
 
   componentDidMount() {
@@ -33,7 +47,43 @@ export default class Passenger extends Component {
     );
   }
 
+  async onChangeDestination(destination) {
+    this.setState({ destination });
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}&input={${destination}}&location=${
+      this.state.latitude
+    },${this.state.longitude}&radius=2000`;
+    const result = await fetch(apiUrl);
+    const jsonResult = await result.json();
+    this.setState({
+      locationPredictions: jsonResult.predictions
+    });
+    console.log(jsonResult);
+  }
+
+  pressedPrediction(prediction) {
+    console.log(prediction);
+    Keyboard.dismiss();
+    this.setState({
+      locationPredictions: [],
+      destination: prediction.description
+    });
+    Keyboard;
+  }
+
   render() {
+    const locationPredictions = this.state.locationPredictions.map(
+      prediction => (
+        <TouchableHighlight
+          key={prediction.id}
+          onPress={() => this.pressedPrediction(prediction)}
+        >
+          <Text style={styles.locationSuggestion}>
+            {prediction.description}
+          </Text>
+        </TouchableHighlight>
+      )
+    );
+
     return (
       <View style={styles.container}>
         <MapView
@@ -46,12 +96,37 @@ export default class Passenger extends Component {
           }}
           showsUserLocation={true}
         />
+        <TextInput
+          placeholder="Enter location.."
+          style={styles.destinationInput}
+          onChangeText={destination =>
+            this.onChangeDestinationDebounced(destination)
+          }
+          value={this.state.destination}
+        />
+        {locationPredictions}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  destinationInput: {
+    borderWidth: 0.5,
+    borderColor: "grey",
+    height: 40,
+    marginTop: 50,
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 5,
+    backgroundColor: "white"
+  },
+  locationSuggestion: {
+    backgroundColor: "white",
+    padding: 5,
+    fontSize: 18,
+    borderWidth: 0.5
+  },
   container: {
     ...StyleSheet.absoluteFillObject
   },
