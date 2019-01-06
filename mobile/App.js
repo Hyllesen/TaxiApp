17176cedd3c77ae7e19bf75614a4ed94895eb10a
Stefan Hyltoft <stefan@Stefans-iMac.local>
2019-01-06 02:02:19 +0100

    Added places autocomplete destination input

--- mobile/App.js
+++ mobile/App.js
@@ -1,53 +1,104 @@
 import React, { Component } from "react";
 import { TextInput, StyleSheet, Text, View } from "react-native";
-import io from "socket.io-client";
+import MapView from "react-native-maps";
+import apiKey from "./google_api_key";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      chatMessage: "",
-      chatMessages: []
+      error: "",
+      latitude: 0,
+      longitude: 0,
+      destination: "",
+      predictions: []
     };
   }
 
   componentDidMount() {
-    this.socket = io("http://192.168.0.27:3000");
-    this.socket.on("chat message", msg => {
-      this.setState({ chatMessages: [...this.state.chatMessages, msg] });
-    });
+    //Get current location and set initial region to this
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      },
+      error => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, maximumAge: 2000, timeout: 20000 }
+    );
   }
 
-  submitChatMessage() {
-    this.socket.emit("chat message", this.state.chatMessage);
-    this.setState({ chatMessage: "" });
+  async onChangeDestination(destination) {
+    this.setState({ destination });
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}
+    &input=${destination}&location=${this.state.latitude},${
+      this.state.longitude
+    }&radius=2000`;
+    try {
+      const result = await fetch(apiUrl);
+      const json = await result.json();
+      this.setState({
+        predictions: json.predictions
+      });
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   render() {
-    const chatMessages = this.state.chatMessages.map(chatMessage => (
-      <Text key={chatMessage}>{chatMessage}</Text>
+    const predictions = this.state.predictions.map(prediction => (
+      <Text style={styles.suggestions} key={prediction.id}>
+        {prediction.description}
+      </Text>
     ));
 
     return (
       <View style={styles.container}>
-        <TextInput
-          style={{ height: 40, borderWidth: 2 }}
-          autoCorrect={false}
-          value={this.state.chatMessage}
-          onSubmitEditing={() => this.submitChatMessage()}
-          onChangeText={chatMessage => {
-            this.setState({ chatMessage });
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
           }}
+          showsUserLocation={true}
         />
-        {chatMessages}
+        <TextInput
+          placeholder="Enter destination..."
+          style={styles.destinationInput}
+          value={this.state.destination}
+          onChangeText={destination => this.onChangeDestination(destination)}
+        />
+        {predictions}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  suggestions: {
+    backgroundColor: "white",
+    padding: 5,
+    fontSize: 18,
+    borderWidth: 0.5,
+    marginLeft: 5,
+    marginRight: 5
+  },
+  destinationInput: {
+    height: 40,
+    borderWidth: 0.5,
+    marginTop: 50,
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 5,
+    backgroundColor: "white"
+  },
   container: {
-    flex: 1,
-    backgroundColor: "#F5FCFF"
+    ...StyleSheet.absoluteFillObject
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
   }
 });
