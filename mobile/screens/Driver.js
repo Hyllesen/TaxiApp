a4ab8413b23beeb011b603ad46b057cb4e967485
Stefan Hyltoft <stefan@Stefans-iMac.local>
2019-01-16 17:58:58 +0100

    Showing request button

---
+++ mobile/screens/Driver.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import {
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  Keyboard,
+  TouchableHighlight
+} from "react-native";
+import MapView, { Polyline, Marker } from "react-native-maps";
+import apiKey from "../google_api_key";
+import PolyLine from "@mapbox/polyline";
+
+export default class Driver extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      destination: "",
+      predictions: [],
+      pointCoords: []
+    };
+  }
+
+  componentDidMount() {
+    //Get current location and set initial region to this
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      },
+      error => console.error(error),
+      { enableHighAccuracy: true, maximumAge: 2000, timeout: 20000 }
+    );
+  }
+
+  render() {
+    let marker = null;
+
+    if (this.state.pointCoords.length > 1) {
+      marker = (
+        <Marker
+          coordinate={this.state.pointCoords[this.state.pointCoords.length - 1]}
+        />
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          ref={map => {
+            this.map = map;
+          }}
+          style={styles.map}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
+          }}
+          showsUserLocation={true}
+        >
+          <Polyline
+            coordinates={this.state.pointCoords}
+            strokeWidth={4}
+            strokeColor="red"
+          />
+          {marker}
+        </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  findDriver: {
+    backgroundColor: "black",
+    marginTop: "auto",
+    margin: 20,
+    padding: 15,
+    paddingLeft: 30,
+    paddingRight: 30,
+    alignSelf: "center"
+  },
+  findDriverText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "600"
+  },
+  suggestions: {
+    backgroundColor: "white",
+    padding: 5,
+    fontSize: 18,
+    borderWidth: 0.5,
+    marginLeft: 5,
+    marginRight: 5
+  },
+  destinationInput: {
+    height: 40,
+    borderWidth: 0.5,
+    marginTop: 50,
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 5,
+    backgroundColor: "white"
+  },
+  container: {
+    ...StyleSheet.absoluteFillObject
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
---
+++ mobile/screens/Passenger.js
@@ -0,0 +1,207 @@
+import React, { Component } from "react";
+import {
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  Keyboard,
+  TouchableHighlight
+} from "react-native";
+import MapView, { Polyline, Marker } from "react-native-maps";
+import apiKey from "../google_api_key";
+import _ from "lodash";
+import PolyLine from "@mapbox/polyline";
+
+export default class Passenger extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      destination: "",
+      predictions: [],
+      pointCoords: []
+    };
+    this.onChangeDestinationDebounced = _.debounce(
+      this.onChangeDestination,
+      1000
+    );
+  }
+
+  componentDidMount() {
+    //Get current location and set initial region to this
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      },
+      error => console.error(error),
+      { enableHighAccuracy: true, maximumAge: 2000, timeout: 20000 }
+    );
+  }
+
+  async getRouteDirections(destinationPlaceId, destinationName) {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/directions/json?origin=${
+          this.state.latitude
+        },${
+          this.state.longitude
+        }&destination=place_id:${destinationPlaceId}&key=${apiKey}`
+      );
+      const json = await response.json();
+      console.log(json);
+      const points = PolyLine.decode(json.routes[0].overview_polyline.points);
+      const pointCoords = points.map(point => {
+        return { latitude: point[0], longitude: point[1] };
+      });
+      this.setState({
+        pointCoords,
+        predictions: [],
+        destination: destinationName
+      });
+      Keyboard.dismiss();
+      this.map.fitToCoordinates(pointCoords, {
+        edgePadding: { top: 20, bottom: 20, left: 20, right: 20 }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async onChangeDestination(destination) {
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}
+    &input=${destination}&location=${this.state.latitude},${
+      this.state.longitude
+    }&radius=2000`;
+    console.log(apiUrl);
+    try {
+      const result = await fetch(apiUrl);
+      const json = await result.json();
+      this.setState({
+        predictions: json.predictions
+      });
+      console.log(json);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  render() {
+    let marker = null;
+    let getDriver = null;
+
+    if (this.state.pointCoords.length > 1) {
+      marker = (
+        <Marker
+          coordinate={this.state.pointCoords[this.state.pointCoords.length - 1]}
+        />
+      );
+      getDriver = (
+        <View style={styles.findDriver}>
+          <View>
+            <Text style={styles.findDriverText}>REQUEST 🚗</Text>
+          </View>
+        </View>
+      );
+    }
+
+    const predictions = this.state.predictions.map(prediction => (
+      <TouchableHighlight
+        onPress={() =>
+          this.getRouteDirections(
+            prediction.place_id,
+            prediction.structured_formatting.main_text
+          )
+        }
+        key={prediction.id}
+      >
+        <View>
+          <Text style={styles.suggestions}>
+            {prediction.structured_formatting.main_text}
+          </Text>
+        </View>
+      </TouchableHighlight>
+    ));
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          ref={map => {
+            this.map = map;
+          }}
+          style={styles.map}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
+          }}
+          showsUserLocation={true}
+        >
+          <Polyline
+            coordinates={this.state.pointCoords}
+            strokeWidth={4}
+            strokeColor="red"
+          />
+          {marker}
+        </MapView>
+        <TextInput
+          placeholder="Enter destination..."
+          style={styles.destinationInput}
+          value={this.state.destination}
+          clearButtonMode="always"
+          onChangeText={destination => {
+            console.log(destination);
+            this.setState({ destination, pointCoords: [] });
+            this.onChangeDestinationDebounced(destination);
+          }}
+        />
+        {predictions}
+        {getDriver}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  findDriver: {
+    backgroundColor: "black",
+    marginTop: "auto",
+    margin: 20,
+    padding: 15,
+    paddingLeft: 30,
+    paddingRight: 30,
+    alignSelf: "center"
+  },
+  findDriverText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "600"
+  },
+  suggestions: {
+    backgroundColor: "white",
+    padding: 5,
+    fontSize: 18,
+    borderWidth: 0.5,
+    marginLeft: 5,
+    marginRight: 5
+  },
+  destinationInput: {
+    height: 40,
+    borderWidth: 0.5,
+    marginTop: 50,
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 5,
+    backgroundColor: "white"
+  },
+  container: {
+    ...StyleSheet.absoluteFillObject
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
