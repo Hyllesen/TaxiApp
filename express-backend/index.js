dc941762b456f0595ae5ed9d2f349a3b3e8be80e
Stefan Hyltoft <stefan@Stefans-iMac.local>
2019-03-09 13:57:10 +0100

    Can now create a simple user

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const User = require("./model/User");
 const PORT = 4000;
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoDbConnectionString =
   "mongodb://testUser:testtest1@ds251332.mlab.com:51332/taxiapp-demo";
 
-app.get("/user", (req, res) => {
+app.get("/users", (req, res) => {
   res.send("You fetched a user!");
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const result = await user.save();
+    res.send(result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 mongoose
---
+++ express-backend/model/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
